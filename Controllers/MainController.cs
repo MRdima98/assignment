511f4ad1786ea5689fa63658567ad974eef83046
dima <dimafrunza69@gmail.com>
2023-10-29 11:33:18 +0100

    Simple json deserialization

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -1,18 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+using System.Text.Json;
 
 namespace assignment.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 public class MainController : ControllerBase {
-    private static readonly string Gotem = "gotem";
-
 
     public MainController() {
     }
 
+    static async Task<List<User>> Fetch() {
+        using ( var httpClient = new HttpClient()){
+            string api = "https://jsonplaceholder.typicode.com/users";
+            var response = await httpClient.GetAsync(api);
+            if (response.IsSuccessStatusCode) {
+              var content = await response.Content.ReadAsStringAsync();
+              Console.WriteLine(content);
+              List<User>? users = JsonSerializer.Deserialize<List<User>>(content);
+              return users;
+            }
+        }
+        return null;
+    }
+
     [HttpGet()]
-    public IEnumerable<string> Get() {
-        return new string[] { "string" };
+    public async Task<List<User>> Get() {
+        await Fetch();
+        return await Fetch();
     }
 }
