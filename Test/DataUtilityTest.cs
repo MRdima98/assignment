6bdc1f16f3792a0a6e165e6d115b88d818a10771
dima <d.frunza@engim.eu>
2023-10-31 08:23:46 +0100

    Set up tests

---
+++ Test/DataUtilityTest.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using assignment.Utilities;
+
+[TestClass]
+public class DataUtilityTest {
+
+    [TestInitialize]
+    public void Initialize() {
+    }
+
+    [TestMethod]
+    public void TestUserFetch() {
+    }
+
+    [TestMethod]
+    public void TestTodoFetch() {
+        Assert.AreEqual(true, true);
+    }
+
+    [TestMethod]
+    public void TestCombineData() {
+        Assert.AreEqual(true, true);
+    }
+
+    [TestMethod]
+    public void TestFilterTodos() {
+        Assert.AreEqual(true, true);
+    }
+}
